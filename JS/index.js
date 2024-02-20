129d7ecb1a848d9bca868ee86d3f142c178c853f
Gurban Mamedov <mike18051996@gmail.com>
2024-02-20 15:02:47 +0400

    Not Finished

---
+++ JS/index.js
@@ -0,0 +1,75 @@
+//Создай три промта, два числа и  арифметический оператор (+,-,/,*),и выполни вычисление. Отработать ошибку если введено неверное значение
+
+// let num1 = parseFloat(prompt("Введите первое число: "));
+// let num2 = parseFloat(prompt("Введите второе число: "));
+// let symbols = prompt("Введите оператор (+, -, *, /): ");
+
+// if (isNaN(num1) || isNaN(num2) || !["+", "-", "*", "/"].includes(symbols)) {
+//   console.log("Введены неправильные значения.");
+// } else {
+//   let result;
+
+//   switch (symbols) {
+//     case "+":
+//       result = num1 + num2;
+//       break;
+//     case "-":
+//       result = num1 - num2;
+//       break;
+//     case "*":
+//       result = num1 * num2;
+//       break;
+//     case "/":
+//       if (num2 !== 0) {
+//         result = num1 / num2;
+//       } else {
+//         console.log("Ошибка. Деление на ноль");
+//         break;
+//       }
+//       break;
+//     default:
+//       console.log("Некорректный оператор");
+//       break;
+//   }
+
+//   console.log('Результат ' + result)
+// }
+
+
+
+
+
+//2 - Добавь промта возможность ввести название фруктов, вебери 5 фруктов 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//3 - С помощью объекта Date, выведи какой сегодня день недели
+
+// let currentDate = new Date();
+
+// let weekDay = currentDate.getDay();
+
+// let weekDays = [
+//   "Воскресенье",
+//   "Понедельник",
+//   "Вторник",
+//   "Среда",
+//   "Четверг",
+//   "Пятница",
+//   "Суббота",
+// ];
+
+// console.log("Сегодня " + weekDays[weekDay]);
